13d51b046910828e366ef397cefaa3f1d2ec8599
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-03-28 21:08:04 +0100

    call the api and recover data from back-end

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,32 @@
-<script setup>
+<script>
+import AppHeader from './components/AppHeader.vue';
+import axios from 'axios';
+const endpoint = 'http://127.0.0.1:8000/api/projects/';
+export default {
+  name: 'Boolfolio',
+  components: { AppHeader },
+  data: () => ({
+    project: []
+
+  }),
+  methods: {
+    fetchProjects() {
+      axios.get(endpoint).then(res => {
+        console.log(res.data)
+      })
+    }
+  },
+  created() {
+    this.fetchProjects();
+  }
+}
 </script>
 
 <template>
+  <AppHeader />
+  <main class="container pt-3">
+  </main>
+
 
 </template>
 
