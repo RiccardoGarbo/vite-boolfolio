3e1d038fa829961ef8d7d593d6b18c6dbeb04052
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-03-28 22:12:31 +0100

    create component projectcard and transfer element on page

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,15 +1,18 @@
 <script>
+import ProjectCard from './ProjectCard.vue';
 export default {
     name: 'AppMain',
+    components: { ProjectCard },
     props: {
         'projects': Array
     }
 }
 </script>
 
-
-
 <template>
+    <div class="container">
+        <ProjectCard v-for="project in projects" :project="project" />
+    </div>
 </template>
 
 
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    name: 'ProjectCard',
+    props: {
+        'project': Object
+    }
+}
+</script>
+
+
+
+<template>
+    <div class="card">
+        <img :src="project.image" class="card-img-top" :alt="project.tite">
+        <div class="card-body">
+            <h5 class="card-title"> {{ project.title }}</h5>
+            <p class="card-text">{{ project.content }}</p>
+        </div>
+    </div>
+</template>
+
+
+
+<style>
+/* Style here */
+</style>
