16e4f9a8bc58b93f212244292c653a574c7fbf7c
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-04-03 15:28:43 +0200

    add other entity to projectcard

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -9,18 +9,27 @@ export default {
 
 
 <template>
-    <div class="card">
-        <img :src="project.image" class="card-img-top" :alt="project.title">
-        <div class="card-body">
-            <div class="d-flex justify-content-between">
-                <h5 class="card-title"> {{ project.title }}</h5>
-                <RouterLink v-if="!isDetail" class="btn btn-info"
-                    :to="{ name: 'project-detail', params: { id: project.id } }">Vedi
-                </RouterLink>
-            </div>
-            <p class="card-text">{{ project.content }}</p>
-        </div>
+    <div class="container">
+        <div class="card">
+            <img :src="project.image" class="card-img-top" :alt="project.title">
+            <div class="card-body">
+                <div class="d-flex justify-content-between">
+                    <h5 class="card-title"> {{ project.title }}</h5>
+                    <RouterLink v-if="!isDetail" class="btn btn-info"
+                        :to="{ name: 'project-detail', params: { id: project.id } }">Vedi
+                    </RouterLink>
+                </div>
+                <p class="card-text">{{ project.content }}</p>
+                <div class="d-flex justify-content-between">
+                    <span>{{ project.type['label'] }}</span>
+                    <div class="d-flex gap-2">
+                        <span v-for="technology in project.technologies">{{ technology['label'] }} </span>
+                    </div>
+                </div>
 
+            </div>
+
+        </div>
     </div>
 
 </template>
